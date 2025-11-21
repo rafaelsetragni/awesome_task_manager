cc359fcd1c3a087c2ae493cc2335b63471a44922
Rafael Setragni <rafaelsetra@gmail.com>
2025-11-21 06:30:00 -0300

    Enhances task observer with manager and task IDs
    Introduces support for managerId and taskId to improve task observation fidelity. Implements factory constructors for flexibility in task association, allowing both multi-task and single-task tracking.

--- lib/src/widgets/task_observer_widget.dart
+++ lib/src/widgets/task_observer_widget.dart
@@ -2,14 +2,51 @@ import 'package:awesome_task_manager/awesome_task_manager.dart';
 import 'package:flutter/cupertino.dart';
 
 class AwesomeTaskObserver<T> extends StatelessWidget {
-  final String? taskId;
+  final String? taskId, managerId;
   final AsyncWidgetBuilder<TaskStatus> builder;
 
-  const AwesomeTaskObserver({required this.builder, super.key, this.taskId});
+  factory AwesomeTaskObserver({
+    required AsyncWidgetBuilder<TaskStatus> builder,
+  }) =>
+      AwesomeTaskObserver._(
+        builder: builder,
+        managerId: null,
+        taskId: null,
+      );
+
+  factory AwesomeTaskObserver.byManagerId({
+    required AsyncWidgetBuilder<TaskStatus> builder,
+    required String managerId,
+  }) =>
+      AwesomeTaskObserver._(
+        builder: builder,
+        managerId: managerId,
+        taskId: null,
+      );
+
+  factory AwesomeTaskObserver.byTaskId({
+    required AsyncWidgetBuilder<TaskStatus> builder,
+    required String taskId,
+  }) =>
+      AwesomeTaskObserver._(
+        builder: builder,
+        managerId: null,
+        taskId: taskId,
+      );
+
+  const AwesomeTaskObserver._({
+    required this.managerId,
+    required this.taskId,
+    required this.builder,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) => StreamBuilder<TaskStatus>(
-        stream: AwesomeTaskManager().getTaskStatusStream(taskId: taskId),
+        stream: managerId != null
+            ? AwesomeTaskManager()
+                .getManagerTaskStatusStream(managerId: managerId)
+            : AwesomeTaskManager().getTaskStatusStream(taskId: taskId),
         builder: (context, snapshot) {
           AwesomeTaskManager()
               .log('AwesomeTaskObserver rebuilt for taskId: $taskId');
